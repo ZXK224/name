d994803fd36e79143f5336de2852edb20a50418e
我没那么差 <105147796+ZXK224@users.noreply.github.com>
2025-08-30 23:49:06 +0800

    Update 云盘兑换.py

--- 云盘兑换.py
+++ 云盘兑换.py
@@ -151,7 +151,7 @@ class YP:
 
 
 if __name__ == "__main__":
-    env_name = 'ydypdh'
+    env_name = 'ydyp'
     token = os.getenv(env_name)
     if not token:
         print(f'⛔️未获取到ck变量：请检查变量 {env_name} 是否填写')
